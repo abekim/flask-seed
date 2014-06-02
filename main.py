5f05c37b4b4fe5ea3b1edd9949a2fb1e23ea6173
Abe Kim <abekim0607@gmail.com>
2014-06-02 13:45:44 -0700

    frontend for task manager finished

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # dev mode
 app.debug = True
 
-# paths
-TEMPLATE_PATH = "templates/"
-
-@app.route("/<name>")
-def hello(name="Person"):
-    return render_template(TEMPLATE_PATH + "hello.html", name=name)
+@app.route("/")
+def hello():
+    return render_template("index.html", title="Welcome")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
